27bbc709a5833a02f3da86d04302f85eceed42f6
cyj <lodong.development@gmail.com>
2025-12-21 19:19:34 +0900

    chore: 버전 1.13.0으로 업데이트

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -19,7 +19,7 @@ import (
 	"health-agent/internal/wsclient"
 )
 
-const version = "1.12.0"
+const version = "1.13.0"
 
 const serviceFile = `[Unit]
 Description=Health Agent - Service Health Check Agent
